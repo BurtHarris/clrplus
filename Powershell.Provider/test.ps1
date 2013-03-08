c58bdb86535a19de303321109e343ec05725f2de
Eric Schultz <wwahammy@gmail.com>
2013-03-08 12:17:15 -0600

    removed unneeded test.ps1 file

--- Powershell.Provider/test.ps1
+++
@@ -1,3 +0,0 @@
-import-module  C:\root\coapp\UniversalFileAccess\output\any\Debug\bin\coapp.universalfileaccess.dll
-#import-module C:\root\coapp\universalfileaccess\UniversalFileAccess\obj\Debug\CoApp.UniversalFileAccess.dll
-get-psdrive
